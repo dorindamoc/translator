5a0e4f19a45158c0f761fc5b05532b24c9b1545b
Dorin Damoc <100198925+dorindamoc@users.noreply.github.com>
2024-01-04 10:26:56 +0100

    api call db update and error handling

---
+++ shared/src/commonMain/kotlin/com/example/translator_kmm/core/domain/util/Resource.kt
@@ -0,0 +1,8 @@
+package com.example.translator_kmm.core.domain.util
+
+
+// Instead of kotlin result class, to make it compatible with swift
+sealed class Resource<T>(val data: T?, throwable: Throwable? = null) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(throwable: Throwable) : Resource<T>(null, throwable)
+}
