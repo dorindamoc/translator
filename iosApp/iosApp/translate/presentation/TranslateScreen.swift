ff892f74c6645996aef73e9ffff361e1dadf0fe2
Dorin Damoc <100198925+dorindamoc@users.noreply.github.com>
2024-01-08 18:42:22 +0100

    ios: wrap the shared viewModel in IOSViewMOdel

---
+++ iosApp/iosApp/translate/presentation/TranslateScreen.swift
@@ -0,0 +1,19 @@
+//
+//  TranslateScreen.swift
+//  iosApp
+//
+//  Created by Dorin Damoc on 08/01/2024.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct TranslateScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TranslateScreen()
+}
