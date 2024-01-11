21c86d743f34990cc6233a3ce2558acffc36c349
Dorin Damoc <100198925+dorindamoc@users.noreply.github.com>
2024-01-11 09:45:04 +0100

    android: VoiceToTextViewModel implementation

---
+++ androidApp/src/main/java/com/example/translator_kmm/android/voice_to_text/presentation/AndroidVoiceToTextViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.translator_kmm.android.voice_to_text.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.translator_kmm.android.voice_to_text.data.AndroidVoiceToTextParser
+import com.example.translator_kmm.voice_to_text.presentation.VoiceToTextEvent
+import com.example.translator_kmm.voice_to_text.presentation.VoiceToTextViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AndroidVoiceToTextViewModel @Inject constructor(
+    private val parser: AndroidVoiceToTextParser
+): ViewModel() {
+    private val viewModel by lazy {
+        VoiceToTextViewModel(parser, viewModelScope)
+    }
+
+    val state = viewModel.state
+    fun onEvent(event: VoiceToTextEvent) {
+        viewModel.onEvent(event)
+    }
+}
